e7ca89b4d4437dfec5c117697f8cd52ebff1ae51
Ramon Chiara <ramonchiara@gmail.com>
2018-09-29 23:06:12 -0300

    Olá, nome! vs Olá, mundo! com HTML, com MIME-type...

--- src/main/java/br/pro/ramon/dcs/jaxrs/hello/OlaResource.java
+++ src/main/java/br/pro/ramon/dcs/jaxrs/hello/OlaResource.java
@@ -2,12 +2,14 @@ package br.pro.ramon.dcs.jaxrs.hello;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 @Path("/ola")
 public class OlaResource {
 
     @GET
+    @Produces("text/html")
     public String getOlaMundo(@QueryParam("nome") String nome) {
         if (nome == null || nome.trim().isEmpty()) {
             nome = "mundo";
