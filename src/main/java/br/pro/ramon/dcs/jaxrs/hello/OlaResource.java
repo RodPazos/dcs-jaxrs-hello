41975f3f33c6a154145de282a389dcbefcff2391
Ramon Chiara <ramonchiara@gmail.com>
2018-09-29 22:59:51 -0300

    Olá, nome!

--- src/main/java/br/pro/ramon/dcs/jaxrs/hello/OlaResource.java
+++ src/main/java/br/pro/ramon/dcs/jaxrs/hello/OlaResource.java
@@ -2,13 +2,14 @@ package br.pro.ramon.dcs.jaxrs.hello;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 @Path("/ola")
 public class OlaResource {
 
     @GET
-    public String getOlaMundo() {
-        return "Olá, mundo!";
+    public String getOlaMundo(@QueryParam("nome") String nome) {
+        return "Olá, " + nome + "!";
     }
 
 }
